c220c765b5b0c526168a8a07cd89c35e3372b8e6
Vanessa Chan <vchan8084@gmail.com>
2020-08-29 20:14:40 -0400

    Updates

---
+++ precache-manifest.0e8dd9e1d3626e2bb2901dbfe999ea58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7bb29ea60666921f55579540468279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b937f07835a6aefea8d",
+    "url": "/static/css/main.55a0ceab.chunk.css"
+  },
+  {
+    "revision": "932f990272970aedfe8e",
+    "url": "/static/js/2.325ea9c3.chunk.js"
+  },
+  {
+    "revision": "0c7f7415c0ec92798fd914561f3edc54",
+    "url": "/static/js/2.325ea9c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b937f07835a6aefea8d",
+    "url": "/static/js/main.ccddb2de.chunk.js"
+  },
+  {
+    "revision": "1a520e85f051a18046bf",
+    "url": "/static/js/runtime-main.e12bf55a.js"
+  }
+]);
